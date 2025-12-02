f22dc101b85266c6be0695b605c6a449cf7139bd
Micahel Tezza <mtezza9@gmail.com>
2025-12-02 13:06:15 +0100

    add: Second AoC Day

--- AOC1/target/debug/deps/AOC1-cacc22eb57d42902.d
+++
@@ -1,5 +0,0 @@
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC1/target/debug/deps/AOC1-cacc22eb57d42902.d: src/main.rs
-
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC1/target/debug/deps/libAOC1-cacc22eb57d42902.rmeta: src/main.rs
-
-src/main.rs:
