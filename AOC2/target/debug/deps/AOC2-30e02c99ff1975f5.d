baf2ca46512dc6515dc3e0c984d8a89543822980
Micahel Tezza <mtezza9@gmail.com>
2025-12-04 11:50:28 +0100

    add: AoC Day 4

--- AOC2/target/debug/deps/AOC2-30e02c99ff1975f5.d
+++
@@ -1,5 +0,0 @@
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC2/target/debug/deps/AOC2-30e02c99ff1975f5.d: src/main.rs
-
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC2/target/debug/deps/libAOC2-30e02c99ff1975f5.rmeta: src/main.rs
-
-src/main.rs:
--- AOC3/target/debug/deps/AOC3-760aaea692b0ce20.d
+++
@@ -1,5 +0,0 @@
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC3/target/debug/deps/AOC3-760aaea692b0ce20.d: src/main.rs
-
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC3/target/debug/deps/libAOC3-760aaea692b0ce20.rmeta: src/main.rs
-
-src/main.rs:
