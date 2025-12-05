845ba8715cf448fbef6f7647bfd0e7e08664476e
Micahel Tezza <mtezza9@gmail.com>
2025-12-05 09:42:35 +0100

    feat: update .gitignore and remove tracked files

--- AOC2/target/debug/deps/AOC2-73ed39ea029dcf1c.d
+++
@@ -1,5 +0,0 @@
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC2/target/debug/deps/AOC2-73ed39ea029dcf1c.d: src/main.rs
-
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC2/target/debug/deps/libAOC2-73ed39ea029dcf1c.rmeta: src/main.rs
-
-src/main.rs:
--- AOC3/target/debug/deps/AOC3-5d3c89c879ab4360.d
+++
@@ -1,5 +0,0 @@
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC3/target/debug/deps/AOC3-5d3c89c879ab4360.d: src/main.rs
-
-/Users/michaeltezza/Documents/AdventOfCode2025/AOC3/target/debug/deps/libAOC3-5d3c89c879ab4360.rmeta: src/main.rs
-
-src/main.rs:
